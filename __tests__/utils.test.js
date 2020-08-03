a56fdf40cbd00d5d6dd5a9ff87db7c237e70653c
XCodeCreatorX <jasminelloyd.code@gmail.com>
2020-08-03 16:20:14 +0100

    completed formatDates() and passed all tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,27 +2,96 @@ const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
 
-// This utility function should be able to take an array (list) of objects and return a new array. Each item in the new array must have its timestamp converted into a Javascript date object. Everything else in each item must be maintained.
+describe("formatDates", () => {
+  test("When input is an empty array, return a new empty array.", () => {
+    expect(formatDates([])).toEqual([]);
+  });
+  test("Make sure the original array isn't mutated", () => {
+    const input = [];
+    formatDates(input)
 
-//hint: Think carefully about how you can test that this has worked - it's not by copying and pasting a sql timestamp from the terminal into your test
+    expect(input).not.toBe([])
+  });
+  test("If the inputted array contains one object, return an array with an object - reformatting the time.", () => {
+    const date = new Date(1542284514171).toDateString();
 
-//Test Ideas:
-// - Empty Array --> Empty Array
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+    ];
 
-// - Does not mutate original array / creates a new array of objects
-// - Converts the timestamp into a javascript data object
-// - Does not change anything else within the objects
+    const output = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: date,
+        votes: 100,
+      },
+    ];
 
-// new Date()
+    expect(formatDates(input)).toEqual(output);
+  });
+  test("If the inputted array contains more than one object, return an array of objects with reformatted times.", () => {
+    const date = new Date(1542284514171).toDateString();
+    const date2 = new Date(1033708614172).toDateString();
 
-describe('formatDates', () => {
-  test('', () => {
-  })
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+      {
+        title: "UNCOVERED: catspiracy to bring down democracy",
+        topic: "cats",
+        author: "rogersop",
+        body: "Bastet walks amongst us, and the cats are taking arms!",
+        created_at: 1033708614172,
+      },
+    ];
+
+    const output = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: date,
+        votes: 100,
+      },
+      {
+        title: "UNCOVERED: catspiracy to bring down democracy",
+        topic: "cats",
+        author: "rogersop",
+        body: "Bastet walks amongst us, and the cats are taking arms!",
+        created_at: date2,
+      },
+    ];
+
+    expect(formatDates(input)).toEqual(output);
+  });
 });
 
-describe('makeRefObj', () => {});
+// This utility function should be able to take an array (list) of objects and return a reference object. The reference object must be keyed by each item's title, with the values being each item's corresponding id. e.g.
+// [{ article_id: 1, title: 'A' }]
+// will become
+// { A: 1 }
 
-describe('formatComments', () => {});
- 
+//Test Notes:
+
+describe("makeRefObj", () => {});
+
+describe("formatComments", () => {});
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,6 +1,17 @@
-exports.formatDates = list => {};
+exports.formatDates = (list) => {
+    let formattedDataArr = [];
+    const dataArr = [...list];
 
-exports.makeRefObj = list => {};
+    for (let i = 0; i < dataArr.length; i++) {
+      const dataObj = dataArr[i];
+      const formattedDate = new Date(dataObj.created_at).toDateString();
+      dataObj.created_at = formattedDate;
+      formattedDataArr.push(dataObj);
+    }
+
+    return formattedDataArr;
+}
+
+exports.makeRefObj = (list) => {};
 
 exports.formatComments = (comments, articleRef) => {};
- 
