75b9bf6fbcc3a9a1637debf3945a88122382a422
XCodeCreatorX <jasminelloyd.code@gmail.com>
2020-08-03 11:39:32 +0100

    Completed step 1 and step 2

---
+++ db/index.js
@@ -0,0 +1,10 @@
+const ENV = process.env.NODE_ENV || "test"
+const testData = require("./data/test-data/index")
+const devData = require("./data/dev-data/index")
+
+const data = {
+  development: devData,
+  test: testData
+};
+
+module.exports = data[ENV]
